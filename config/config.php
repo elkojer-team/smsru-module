6312932a028e290a4d82d7a870aecf7c40ce11a6
HamasakiBrain <herovuwbdal@gmail.com>
2025-01-07 17:50:56 +0300

    Fix

--- config/config.php
+++ config/config.php
@@ -2,13 +2,13 @@
 
 return [
     'api_key' => [
-        'value' => Settings::get('integrations.smsru.api_key'),
+        'value' => config('integrations.smsru.token'),
         'type' => 'text',
         'required' => true,
         'description' => 'Токен'
     ],
     'type' => [
-        'value' => Settings::get('integrations.smsru.type', 'sms'),
+        'value' => config('integrations.smsru.type', 'sms'),
         'type' => 'select',
         'values' => \Modules\SMSRU\App\Enums\Types::asSelectArray(),
         'required' => true,
